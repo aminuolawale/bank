a7c85b0e7b1a6ca5e05c11640081bb01681c3195
Mohammed Aminu <aminuolawalekan@gmail.com>
2021-03-27 02:17:42 +0100

    added transactions

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -9,19 +9,20 @@ import (
 	_ "github.com/aminuolawale/bank/util"
 	_ "github.com/lib/pq"
 )
-
-
-var testQueries *Queries
 var	dbDriver = os.Getenv("POSTGRES_DB_DRIVER")
 var	dbSource = os.Getenv("POSTGRES_DB_SOURCE")
 
 
+var testQueries *Queries
+var testDB *sql.DB
+
 
 func TestMain(m *testing.M){
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db!", err)
 	}
-	testQueries = New(conn)
+	testQueries = New(testDB)
 	os.Exit(m.Run())
 }
